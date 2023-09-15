03b47a1e4ef6e50b7c0165f0d1d339eacadb7e80
Sai Kumar Kanuri <saikumar.kanuri.ksvs@gmail.com>
2023-09-15 21:57:30 +0530

    final

--- src/Component/TimelineTweet/TimelineTweet.js
+++ src/Component/TimelineTweet/TimelineTweet.js
@@ -1,13 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-import { useEffect, useState } from "react";
-
-import { useSelector } from "react-redux/es/hooks/useSelector";
+import { useSelector } from "react-redux";
 import Tweet from "../Tweet/Tweet";
 
 const TimelineTweet = () => {
-
     const [timeLine, setTimeLine] = useState(null);
 
     const { currentUser } = useSelector((state) => state.user);
@@ -29,16 +26,18 @@ const TimelineTweet = () => {
     }, [currentUser._id]);
 
     console.log("Timeline", timeLine);
-
     return (
         <div className="mt-6">
-            {timeLine && timeLine.map((tweet) => (
-                <div key={tweet._id} className="p-2 ">
-                    <Tweet tweet={tweet} setData={setTimeLine } />
-                </div>
-            ))}
+            {timeLine &&
+                timeLine.map((tweet) => {
+                    return (
+                        <div key={tweet._id} className="p-2">
+                            <Tweet tweet={tweet} setData={setTimeLine} />
+                        </div>
+                    );
+                })}
         </div>
-    )
-}
+    );
+};
 
 export default TimelineTweet;
